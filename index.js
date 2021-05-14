c7ec357444fb43796ce28dbc957f747d695ce6f9
hgaskin <37593078+hgaskin@users.noreply.github.com>
2021-05-14 17:52:44 -0400

    add PORT env for listen express

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.use(
 
 app.use(cookieParser());
 
-app.listen(5000, () => console.log("Server (backend) started on port 5000!"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server (backend) started on port: ${PORT}`));
 
 //setting up a GET request
 
