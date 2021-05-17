82eca2ac08ada07a01467885d50a98febf2b33d1
hgaskin <37593078+hgaskin@users.noreply.github.com>
2021-05-17 10:31:17 -0400

    index.js comments

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const dotenv = require("dotenv");
 
 const cors = require("cors");
 
-// parse cookie data
+// parse cookie data //
+
 const cookieParser = require("cookie-parser");
 const { get } = require("./routers/userRouter");
 
 dotenv.config();
 
-// setup express server
+// =========== setup express server =========== //
 
 const app = express();
 
-//Setup CORS to allow CROSS ORIGIN >>>> "app.use(cors());" <<<< commmunication between Server and Client side of APP ///
+// setup CORS to allow CROSS ORIGIN >>>> "app.use(cors());" <<<< commmunication between Server and Client side of APP ///
 
 app.use(express.json());
-// ================ Middleware =======
+
+// ================ middleware ================== //
+
 app.use( 
     cors({
         origin: ["http://localhost:3000", "https://ifinance-hgaskin.netlify.app"],
@@ -32,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server (backend) started on port: ${PORT}`));
 
-//setting up a GET request
+// setting up a GET request
 
 // setting up Routers MiddleWare
 
